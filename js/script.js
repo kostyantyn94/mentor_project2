a7b7043214a965aa99a7e8b4f6416781e9dfef2b
Kostya <user.name>
2023-07-19 01:01:43 +0300

    two

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 const globalState = {
     name: {
         text: '',
-        error: ''
+        error: 'none'
     },
     email: {
         text: '',
-        error: ''
+        error: 'none'
     },
     phone: {
         text: '',
-        error: ''
+        error: 'none'
+    },
+    plan: {
+        type: '',
+        time: ''
     }
 }
 
@@ -28,8 +32,13 @@ error_block_phone.style.cssText = "font-size: 14px; color: red; font-weight: 700
 
 const info_inputs = document.querySelectorAll('.info__input');
 
+const info_btn = document.querySelector('.info__btn')
 
-info_inputs.forEach(element => {
+
+info_btn.disabled = true
+
+
+for (let element of info_inputs) {
     element.addEventListener('input', () => {
     
         if (element.name == 'name') {
@@ -62,16 +71,16 @@ info_inputs.forEach(element => {
             }
         }
         if (element.name == 'email') {
-            if(!(/.*?@.*?\..+?/.test(element.value))) {
-                element.style.outline = "1px solid red"
-                element.before(error_block_email);
-                globalState.email.error = "Wrong email format";
-                error_block_email.innerHTML = globalState.email.error;
-            }
-            else if (element.value.length == 0) {
+            if (element.value.length == 0) {
                 element.style.outline = "1px solid red"
                 element.before(error_block_email);
                 globalState.email.error = "Field can not be empty";
+                error_block_email.innerHTML = globalState.email.error;
+            }
+            else if(!(/.*?@.*?\..+?/.test(element.value))) {
+                element.style.outline = "1px solid red"
+                element.before(error_block_email);
+                globalState.email.error = "Wrong email format";
                 error_block_email.innerHTML = globalState.email.error;
             }
             else{
@@ -83,7 +92,13 @@ info_inputs.forEach(element => {
             }
         }
         if (element.name == 'phone') {
-            if(!(/^\+/.test(element.value))) {
+            if (element.value.length == 0) {
+                element.style.outline = "1px solid red"
+                element.before(error_block_phone);
+                globalState.phone.error = "Field can not be empty";
+                error_block_phone.innerHTML = globalState.phone.error;
+            }
+            else if(!(/^\+/.test(element.value))) {
                 element.style.outline = "1px solid red"
                 element.before(error_block_phone);
                 globalState.phone.error = "Your phone number must start with '+'";
@@ -107,12 +122,6 @@ info_inputs.forEach(element => {
                 globalState.phone.error = "Phone number can not be longer than 15 characters";
                 error_block_phone.innerHTML = globalState.phone.error;
             }
-            else if (element.value.length == 0) {
-                element.style.outline = "1px solid red"
-                element.before(error_block_phone);
-                globalState.phone.error = "Field can not be empty";
-                error_block_phone.innerHTML = globalState.phone.error;
-            }
             else{
                 error_block_phone.innerHTML = ''
                 error_block_phone.remove()
@@ -122,5 +131,80 @@ info_inputs.forEach(element => {
                 
             }
         }
+
+        if (globalState.name.error == '' && globalState.email.error == '' && globalState.phone.error == '') {
+            info_btn.disabled = false
+        }
+        else 
+        {
+            info_btn.disabled = true
+        }
+
+    })
+}
+
+const menu_number = document.querySelectorAll('.menu__number');
+
+const info = document.querySelector('.info');
+const plan = document.querySelector('.plan');
+
+
+info_btn.onclick = (e) => {
+    e.preventDefault()
+    info.classList.add('hidden')
+    plan.classList.remove('hidden')
+    for (let elem of menu_number) {
+        if (elem.innerHTML == '2') {
+            elem.classList.add('menu__number_active')
+        }
+        else {
+            elem.classList.remove('menu__number_active')
+        }
+    }
+}
+
+
+const plan_type = document.querySelectorAll('.plan__item');
+let activePlan = null;
+
+
+plan_type.forEach(elem =>{
+    elem.addEventListener('click', (e) => {
+
+        e.currentTarget.classList.add("plan__item_active");
+
+        if ((activePlan !== null && activePlan !== e.currentTarget)) {
+            activePlan.classList.remove("plan__item_active");
+          }
+
+        activePlan = e.currentTarget;
     })
 })
+
+const switcher = document.querySelector('.plan__slider');
+const month = document.querySelector('.plan__time-month');
+const year = document.querySelector('.plan__time-year');
+
+
+switcher.onclick = () => {
+    if(month.classList.contains('plan__time_active')) {
+        month.classList.remove('plan__time_active');
+        year.classList.add('plan__time_active');
+    }
+    else {
+        month.classList.add('plan__time_active');
+        year.classList.remove('plan__time_active');
+    }
+
+}
+
+
+
+const plan_back = document.querySelector('.plan__btns-back');
+
+const plan_next = document.querySelector('.plan__btns-next');
+
+plan_back.onclick = () => {
+    info.classList.remove('hidden')
+    plan.classList.add('hidden')
+}
